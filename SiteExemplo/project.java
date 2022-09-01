42a13d66e4641211c54fba4af559363b2ff28742
devrenj <110774837+devrenj@users.noreply.github.com>
2022-08-31 23:02:01 -0300

    Criei um arquivo HTML e um JAVA
    Página inicial html e arquivo java simples

---
+++ SiteExemplo/project.java
@@ -0,0 +1,10 @@
+package testandoarquivojava;
+
+public class project {
+
+public static void main(String[] args) {
+    String teste = "Apenas testando Java, sem package";
+    System.Out.Println(teste);
+
+    }
+}
